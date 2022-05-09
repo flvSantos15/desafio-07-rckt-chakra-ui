5f389ac8e3eabc21c9531ca881d81d18e2c4d116
flvSantos15 <flvSantos300@gmail.com>
2022-05-08 22:12:15 -0300

    changed api baseUrl to the .json file in heroku

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+// export const api = axios.create({
+//   baseURL: 'http://localhost:3333'
+// })
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://world-trip-server.herokuapp.com/'
 })
